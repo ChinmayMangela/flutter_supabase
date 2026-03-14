b0720453d637e1d355153f85e7ecbd5081b61746
chinmay <mangelachinmay7@gmail.com>
2026-03-14 16:47:00 +0530

    Implement `EndUserModel` with JSON serialization
    - Extend `EndUserModel` from the `EndUser` entity.
    - Add `fromJson` factory method with default values for name, email, and createdAt.
    - Add `toJson` method to convert the model into a map for data persistence.

--- lib/features/auth/data/model/end_user_model.dart
+++ lib/features/auth/data/model/end_user_model.dart
@@ -1,5 +1,33 @@
+import 'package:flutter_supabase/features/auth/domain/entity/end_user.dart';
 
+class EndUserModel extends EndUser {
+  const EndUserModel({
+    required super.id,
+    required super.name,
+    required super.email,
+    required super.password,
+    required super.createdAt,
+  });
 
-class EndUserModel {
+  factory EndUserModel.fromJson(Map<String, dynamic> json) {
+    return EndUserModel(
+      id: json['id'],
+      name: json['name'] as String? ?? 'unknown',
+      email: json['email'] as String? ?? '',
+      password: json['password'],
+      createdAt: json['created_at'] != null
+          ? DateTime.parse(json['created_at'] as String)
+          : DateTime.now(),
+    );
+  }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'created_at': createdAt.toIso8601String(),
+    };
+  }
 }
